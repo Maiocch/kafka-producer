f0b2feb05cca0b3b99e0b8c4f8ec8b7b2a6a91b2
Valerio Maiocchi <valerio.maiocchi94@gmail.com>
2021-08-24 14:50:38 +0200

    no message

--- src/main/java/com/example/demo/controller/ProducerController.java
+++ src/main/java/com/example/demo/controller/ProducerController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,12 @@ public class ProducerController {
 	
 	@GetMapping("/publish/{msg}")
 	public String createMessage(@PathVariable("msg") String mex) {
-		
+
+		ObjectMapper mapper = Jackson2ObjectMapperBuilder.xml().build();
+		ObjectMapper jsonMapper = Jackson2ObjectMapperBuilder.json().build();
+
+
+
 		String status = "OK";
 		try {
 			producer.sendMessage(mex);
